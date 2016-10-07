cc798bae8d5643fdf2468c3d611ce5c72611948e
Gabriel Felipe Soares <gabrielfs7@gmail.com>
2016-10-07 15:47:19 -0300

    creating doc generation files

--- public/index.php
+++ public/index.php
@@ -1,2 +1,14 @@
-<?php
-require __DIR__ . '/../vendor/autoload.php';
+<?php require __DIR__ . '/../vendor/autoload.php'; ?>
+<html>
+    <head>
+        <title>Clean Code Workshop</title>
+    </head>
+    <body>
+        <h1>Clean Code Workshop</h1>
+        <ul>
+            <li>
+                <a href="phpdoc">Documentation (PHPDoc)</a>
+            </li>
+        </ul>
+    </body>
+</html>
