f967cbd6f1dee734b8accd5ef9d0bf7005744c40
Gabriel Felipe Soares <gabrielfs7@gmail.com>
2016-10-26 14:13:11 -0200

    master: refactoring

--- public/index.php
+++ public/index.php
@@ -1,17 +1,54 @@
-<?php require __DIR__ . '/../vendor/autoload.php'; ?>
-<html>
-    <head>
-        <title>Clean Code Workshop</title>
-    </head>
-    <body>
-        <h1>Clean Code Workshop</h1>
-        <ul>
-            <li>
-                <a href="phpdoc">Documentation (PHPDoc)</a>
-            </li>
-            <li>
-                <a href="phpmd">Mess Detector (PHPMD)</a>
-            </li>
-        </ul>
-    </body>
-</html>
+<?php
+
+require __DIR__ . '/../vendor/autoload.php';
+
+use Symfony\Component\Routing\RouteCollection;
+use Symfony\Component\Routing\Route;
+use \Symfony\Component\HttpFoundation\Response;
+use \Symfony\Component\HttpFoundation\Request;
+use \Symfony\Component\Routing\RequestContext;
+use \Symfony\Component\Routing\Matcher\UrlMatcher;
+use \Symfony\Component\HttpKernel\Controller\ControllerResolver;
+use \Symfony\Component\HttpKernel\Controller\ArgumentResolver;
+use \GSoares\CleanCode\Application\Controller\IndexController;
+use \Symfony\Component\Routing\Exception\ResourceNotFoundException;
+
+$request = Request::createFromGlobals();
+
+$route = new Route(
+    '',
+    [
+        'year' => null,
+        '_controller' => [
+            new IndexController(),
+            'indexAction'
+        ],
+    ]
+);
+
+$routes = new RouteCollection();
+$routes->add('route_name', $route);
+
+$context = new RequestContext();
+$context->fromRequest($request);
+
+$matcher = new UrlMatcher($routes, $context);
+
+$controllerResolver = new ControllerResolver();
+$argumentResolver = new ArgumentResolver();
+
+try {
+    $request->attributes
+        ->add($matcher->match($request->getPathInfo()));
+
+    $controller = $controllerResolver->getController($request);
+    $arguments = $argumentResolver->getArguments($request, $controller);
+
+    $response = call_user_func_array($controller, $arguments);
+} catch (ResourceNotFoundException $e) {
+    $response = new Response('Not Found', 404);
+} catch (Exception $e) {
+    $response = new Response('An error occurred', 500);
+}
+
+$response->send();
---
+++ src/GSoares/CleanCode/Application/Controller/IndexController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace GSoares\CleanCode\Application\Controller;
+
+/**
+ * @author Gabriel Felipe Soares <gabrielfs7@gmail.com>
+ */
+class IndexController
+{
+    public function indexAction()
+    {
+        exit('TEST...');
+    }
+}
