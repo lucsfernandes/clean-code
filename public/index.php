a3a328b0cd071b23ac1d3820528f4e764e471a53
Gabriel Felipe Soares <gabrielfs7@gmail.com>
2016-10-27 10:32:17 -0200

    master: refactoring

--- public/index.php
+++ public/index.php
@@ -2,61 +2,17 @@
 
 require __DIR__ . '/../vendor/autoload.php';
 
-use Symfony\Component\Routing\RouteCollection;
-use Symfony\Component\Routing\Route;
-use \Symfony\Component\HttpFoundation\Response;
-use \Symfony\Component\HttpFoundation\Request;
-use \Symfony\Component\Routing\RequestContext;
-use \Symfony\Component\Routing\Matcher\UrlMatcher;
-use \Symfony\Component\HttpKernel\Controller\ControllerResolver;
 use \Symfony\Component\HttpKernel\Controller\ArgumentResolver;
-use \Symfony\Component\Routing\Exception\ResourceNotFoundException;
 use \Symfony\Component\DependencyInjection\ContainerBuilder;
 use \Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
 use \Symfony\Component\Config\FileLocator;
 
-$loader = new XmlFileLoader(
-    $container = new ContainerBuilder(),
-    new FileLocator(__DIR__ . '/../di')
-);
+$fileLocator = new FileLocator(__DIR__ . '/../di');
+$container = new ContainerBuilder();
+
+$loader = new XmlFileLoader($container, $fileLocator);
 $loader->load('all.xml');
 
-$request = Request::createFromGlobals();
-
-$route = new Route(
-    '',
-    [
-        'year' => null,
-        '_controller' => [
-            $container->get('controller.index'),
-            'indexAction'
-        ],
-    ]
-);
-
-$routes = new RouteCollection();
-$routes->add('route_name', $route);
-
-$context = new RequestContext();
-$context->fromRequest($request);
-
-$matcher = new UrlMatcher($routes, $context);
-
-$controllerResolver = new ControllerResolver();
-$argumentResolver = new ArgumentResolver();
-
-try {
-    $request->attributes
-        ->add($matcher->match($request->getPathInfo()));
-
-    $controller = $controllerResolver->getController($request);
-    $arguments = $argumentResolver->getArguments($request, $controller);
-
-    $response = call_user_func_array($controller, $arguments);
-} catch (ResourceNotFoundException $e) {
-    $response = new Response('Not Found', 404);
-} catch (Exception $e) {
-    $response = new Response('An error occurred', 500);
-}
-
-$response->send();
+$loader = $container->get('app.controller_loader');
+$loader->setContainer($container);
+$loader->loadResponse();
---
+++ src/GSoares/CleanCode/Application/Controller/Loader.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace GSoares\CleanCode\Application\Controller;
+
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\ContainerAwareTrait;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Controller\ArgumentResolver;
+use Symfony\Component\HttpKernel\Controller\ControllerResolver;
+use Symfony\Component\Routing\Exception\ResourceNotFoundException;
+use Symfony\Component\Routing\Loader\XmlFileLoader;
+use Symfony\Component\Routing\Matcher\UrlMatcher;
+use Symfony\Component\Routing\RequestContext;
+use Symfony\Component\Routing\RouteCollection;
+
+class Loader
+{
+
+    use ContainerAwareTrait;
+
+    public function loadResponse()
+    {
+        $request = Request::createFromGlobals();
+
+        $fileLocator = new FileLocator(__DIR__ . '/../../../../../di');
+
+        $xmlFileLoader = new XmlFileLoader($fileLocator);
+
+        $routeCollection = $xmlFileLoader->load('routes.xml');
+
+        $this->configureRoutes($routeCollection);
+
+        $context = new RequestContext();
+        $context->fromRequest($request);
+
+        $matcher = new UrlMatcher($routeCollection, $context);
+
+        $controllerResolver = new ControllerResolver();
+        $argumentResolver = new ArgumentResolver();
+
+        try {
+            $request->attributes
+                ->add($matcher->match($request->getPathInfo()));
+
+            $controller = $controllerResolver->getController($request);
+            $arguments = $argumentResolver->getArguments($request, $controller);
+
+            return call_user_func_array($controller, $arguments);
+        } catch (ResourceNotFoundException $e) {
+            return new Response('Not Found', 404);
+        } catch (\Exception $e) {
+            return new Response("An error occurred: {$e->getMessage()}", 500);
+        }
+    }
+
+    /**
+     * @param RouteCollection $routeCollection
+     */
+    private function configureRoutes(RouteCollection $routeCollection)
+    {
+        foreach ($routeCollection as $route) {
+            list($controllerId, $action) = explode(':', $route->getDefault('_controller'));
+
+            $route->setDefault('_controller', [$this->container->get($controllerId), $action]);
+        }
+    }
+}
